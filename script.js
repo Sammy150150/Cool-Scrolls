f62c8f1ea3646239edad05fce8e17edbb9bdb94c
Sammy150150 <138682567+Sammy150150@users.noreply.github.com>
2024-03-13 18:41:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
+
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     if (entry.isIntersecting) {
-      entry.target.classList.add('show');
-    } else {
-      entry.target.classList.remove('show');
+      if (entry.target.classList.contains('hide-from-left')) {
+      entry.target.classList.add('show-from-left');
+    }
+      else if (entry.target.classList.contains('hide-from-right')){
+        entry.target.classList.add('show-from-right');
+      }
+      else if (entry.target.classList.contains('hide-from-bottom')) {
+        entry.target.classList.add('show-from-bottom');
+      }
+    }
+    else {
+      entry.target.classList.remove('show-from-left');
+      entry.target.classList.remove('show-from-right');
+      entry.target.classList.remove('show-from-bottom');
     }
   });
 });
 
-const hiddenElements = document.querySelectorAll('.hide');
+const hiddenElements = document.querySelectorAll('.text');
 
 hiddenElements.forEach((el) => observer.observe(el));
